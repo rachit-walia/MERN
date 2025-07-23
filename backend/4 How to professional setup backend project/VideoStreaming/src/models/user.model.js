2e56b2dea0707b194d26f3a056e47f264d91999c
rachit-walia <rachitwalia3@gmail.com>
2025-07-23 17:40:43 +0530

    usermodel

--- backend/4 How to professional setup backend project/VideoStreaming/src/models/user.model.js
+++ backend/4 How to professional setup backend project/VideoStreaming/src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
   {
@@ -18,7 +20,7 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -47,5 +49,48 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save",async function (){
+
+  if(!(this.isModified("password"))){
+      return next();
+  }
+
+  this.password = bcrypt.hash(this.password, 10)
+  next()
+
+})
+userSchema.methods.isPasswordCorrect = async function (password){
+   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign({
+    _id : this._id,
+    email: this.email,
+    username: this.username,
+    fullName: this.fullName
+  },
+process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  }
+)
+}
+
+userSchema.methods.generateRefreshoken = function(){
+  return jwt.sign({
+    _id : this._id,
+    email: this.email,
+    username: this.username,
+    fullName: this.fullName
+  },
+process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+  }
+)
+}
+
 
 export const User = mongoose.model("User", userSchema);
