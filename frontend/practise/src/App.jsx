686921c4ebe847235c2df527cc6ec61e47ea0c57
rachit-walia <rachitwalia3@gmail.com>
2025-08-26 22:25:14 +0530

    react basics

---
+++ frontend/practise/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header"
+function Apple(){
+  return (
+    
+    <div>
+      <Header/>
+      <h1>Hello World1</h1>
+    <h1>step by step</h1></div>
+  )
+}
+export default Apple;
---
+++ frontend/practise/src/Header.jsx
@@ -0,0 +1,6 @@
+function Header(){
+    return <div>
+        <h1>Hello React</h1>
+        <h3>Hllo walia</h3>
+    </div>
+}
